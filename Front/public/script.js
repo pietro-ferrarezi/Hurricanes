2327f75a5cc58c81e554adb761dbca9f16699709
sytux-boop <sattu.santitt@gmail.com>
2026-03-10 18:37:51 -0300

    V0

---
+++ Front/public/script.js
@@ -0,0 +1,27 @@
+let timeoutCopiedConfirmation
+const payload = "00020126580014BR.GOV.BCB.PIX0136c12ec579-60a6-4af8-b42b-79c1b30170de52040000530398654045.005802BR5923Pietro Diosti Ferrarezi6009SAO PAULO62140510LBhx5LkZ5g6304E7B7"
+
+async function copyPayload() {
+    try {
+        clearTimeout(timeoutCopiedConfirmation)
+        await navigator.clipboard.writeText(payload);
+        displayControll(".confirmed-copied-div", "block")
+        timeoutCopiedConfirmation = setTimeout(() => {
+            displayControll(".confirmed-copied-div", "none")
+        }, 2000)
+    } catch (error) {
+        console.log(`Erro?! (Function: copyPayload()): ${error.message}`);
+    }
+}
+
+function displayControll(element, display) {
+    document.querySelector(element).style.display = display
+}
+
+function goto_donate() {
+    try {
+        window.location.pathname = "/donate"
+    } catch (error) {
+        console.log(`Erro?! (Function: goto_donate()): ${error.message}`)
+    }
+}
---
+++ src/app/main.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const dotenv = require("dotenv")
+dotenv.config()
+const env = process.env
+const path = require("path")
+
+const app = express()
+app.use(express.static(path.join(__dirname, "../../Front/public")))
+const PORT = env.PORT || 3030
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../Front/views", "index.html"))
+})
+
+app.get("/donate", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../Front/views", "donate_area.html"))
+})
+
+app.listen(PORT, () => console.log(`Server Running! Link: http://localhost:${PORT}`))
